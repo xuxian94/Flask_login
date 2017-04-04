16c872983859ab40ade955cb8628bd7dfceb7ff3
xuxian94 <xuxian@xuxiandeMacBook-Pro.local>
2017-04-04 17:21:26 +0800

    读取mysql用户信息

--- config.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-# -*- coding:utf8 -*-
-
-CSRF_ENABLED = True
-SECRET_KEY = 'you-will-never-guess'
-# 登录root
-SQLALCHEMY_DATABASE_URI = 'mysql://root:123456@localhost:3306/text2'
-SQLALCHEMY_COMMIT_ON_TEARDOWN = True
-SQLALCHEMY_TRACK_MODIFICATIONS = True
